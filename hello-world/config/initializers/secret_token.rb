b94a278176a91f9b33ad6f85fc0b03a268b94d9b
Hien Tran <htran@eastern.edu.au>
2024-05-16 11:29:42 +1000

    Created hello world apps
    + update gem file to have the right version of sqlite3 to work with ruby 2.3.8
    + update to have the rubyracer
    + update to have execjs version 2.7.0

---
+++ hello-world/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '3beb196fc4ef5bef7b6c3a656cb8a1ef9c8450e9e814a877f1b4aa22daae5162ac7c6e443a7c01b74b2c1f7163f20f3940d3e02ca5bf222dfb453bbdf603f815'
