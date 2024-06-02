804437c4af70fc685146a230b04d0ee4acd3773f
Hien Tran <htran@eastern.edu.au>
2024-06-03 09:56:37 +1000

    [working on alpha blog]

---
+++ alpha-blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlphaBlog::Application.config.secret_token = '07795af373c53dd00433228bdc45a5207f799ccf18f35ef253fadbbb3e0d457afc4436469afd7be7430a155c77ed0143d6e0fd86a4233c8b01ae1a762d329b2b'
