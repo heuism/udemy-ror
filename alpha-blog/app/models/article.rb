cb7a1ef767a7913583056935bd7ff83e7abbaefb
Hien Tran <htran@eastern.edu.au>
2024-06-03 11:35:50 +1000

    [alpha-blog] validation for fields

--- alpha-blog/app/models/article.rb
+++ alpha-blog/app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :description
+  validates :title, presence: true, length: {minimum:6, maximum:100}
+  validates :description, presence: true, length: {minimum:10, maximum:300}
 end
