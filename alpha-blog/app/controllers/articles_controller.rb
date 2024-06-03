cdfc56aaada7fe65331c4b19936e33bb5a5979f4
Hien Tran <htran@eastern.edu.au>
2024-06-03 12:12:09 +1000

    [alpha-blog] learn about show and params as well as byebug to debug code

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -13,12 +13,13 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    byebug
     @article = Article.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @article }
-    end
+    # respond_to do |format|
+    #   format.html # show.html.erb
+    #   format.json { render json: @article }
+    # end
   end
 
   # GET /articles/new
