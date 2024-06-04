c01d9c34ad300b606a84bb292d0e6848a163b34d
Hien Tran <htran@eastern.edu.au>
2024-06-04 14:42:33 +1000

    [alpha-blog] remove byebug

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
-    byebug
     @article = Article.find(params[:id])
 
     # respond_to do |format|
